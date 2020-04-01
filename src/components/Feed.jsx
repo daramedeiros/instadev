533e11776698f3702c332fa9d95c1572958e1023
Dara Medeiros <daraamedeiros@outlook.com>
2020-04-01 09:06:28 -0300

    update feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,23 +3,12 @@ import './Feed.scss';
 import Post from './Post';
 
 class Feed extends React.Component {
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      users: []
-    }
-  }
-  componentDidMount(){
-    console.log('Montou');
-  }
     render() {
-      const { users } = this.state;
-
         return(
         <div className="container">
           <section className="feed">
-          {users.map((user) => <Post data={users}/>)}
+          <Post />
           </section>
         </div>
         );
